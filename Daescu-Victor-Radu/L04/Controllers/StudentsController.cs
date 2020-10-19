eb04ebcd3b50a5ba0642871e55ab876393447150
Victor Daescu <victor_rvd@yahoo.com>
2020-10-19 12:00:30 +0300

    Add files via upload

---
+++ Daescu-Victor-Radu/L04/Controllers/StudentsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace L04_wapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private IStudentsRepository _studentsRepository;
+
+
+        public StudentsController(IStudentsRepository studentsRepository)
+        {
+            _studentsRepository = studentsRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+           return null;
+            
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+    }
+}
